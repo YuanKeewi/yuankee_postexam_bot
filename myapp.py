ca5bcf0e6ff0c9b754b14f3ec00616a41b5ec0f4
Clover <69779113+dragoniteclover111@users.noreply.github.com>
2020-10-29 18:34:32 +0800

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -14,16 +14,32 @@ while True:
     
     if each_word == "happy":
       feelings_list.append("happy")
-      encouragement_list.append("keep smiling")
-      counter += 1
+      encouragement_list.append("to keep smiling")
+      counter -= 1
     if each_word == "bored":
       feelings_list.append("bored")
-      encouragement_list.append("Find something fun to do")
+      encouragement_list.append("to find something fun to do")
       counter += 1
     if each_word == "tired":
       feelings_list.append("tired")
-      encouragement_list.append("get some sleep")
+      encouragement_list.append("to get some sleep")
       counter += 1
+    if each_word == "sad":
+      feelings_list.append("sad")
+      encouragement_list.append("to cheer up as the storm will pass"
+      counter += 1
+    if each_word == "worried":
+      feelings_list.append("worried")
+      encouragement_list.append("to take a deep breath and calm down")
+      counter += 1
+    if each_word == "lonely":
+      feelings_list.append("lonely")
+      encouragement_list.append("that you're not alone and there would always be someone to cheer you on")
+      counter += 1
+    if each_word == "fine":
+      feelings_list.append("fine")
+      encouragement_list.append("to stay this way and be positive")
+      counter -= 1
 
   if counter == 0:
     
@@ -31,19 +47,19 @@ while True:
 
   elif counter == 1:
     
-      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember that "+ encouragement_list[0] + "! Hope you feel better :)"  
+      output = "It seems that you are feeling quite " + feelings_list[0] + ". However, do remember "+ encouragement_list[0] + "! Hope you feel better :)"  
 
   else:
 
     feelings = ""    
     for i in range(len(feelings_list)-1):
       feelings += feelings_list[i] + ", "
-    feelings += "and " + feelings_list[-1]
+    feelings += "" + feelings_list[-1]
     
     encouragement = ""    
     for j in range(len(encouragement_list)-1):
       encouragement += encouragement_list[i] + ", "
-    encouragement += "and " + encouragement_list[-1]
+    encouragement += "" + encouragement_list[-1]
 
     output = "It seems that you are feeling quite " + feelings + ". Please always remember "+ encouragement + "! Hope you feel better :)"
 
